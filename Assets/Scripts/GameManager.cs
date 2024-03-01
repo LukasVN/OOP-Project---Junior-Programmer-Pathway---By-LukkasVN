c2bbe9b4ad5dffc43a1aca90bfcee4ea22c2b783
Lucas Varela Negro <lucasvnbusiness@gmail.com>
2024-03-01 01:37:04 +0100

    Forgot 1 annotation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public TextMeshProUGUI coinsText;
+    //ENCAPSULATION
     public int coinCounter {get; set;}
     public int targetFPS = 144;
 
